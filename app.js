b05fded72a79c589cf409eb8bc8b6a85060c716d
Jayesh Bhole <103045705+jayeshbhole05@users.noreply.github.com>
2023-03-02 01:24:16 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,11 +3,19 @@ const mongoose = require("mongoose");
 const router = require("./routes/book-routes");
 const cors = require("cors");
 const app = express();
+const {fileURLToPath} = require("url");
+
+const __filename = fileURLToPath(import.meta.url);
+const _dirname = path.dirname(_filename);
 
 // Middlewares
 app.use(express.json());
 app.use(cors());
 app.use("/books", router); // localhost:5000/books
+
+app.use("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 mongoose
   .connect(
